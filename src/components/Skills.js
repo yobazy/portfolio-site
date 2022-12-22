f39dcb6d9acf30d512601154038c490ec5cde6d8
Baz <king.bazil.khan@gmail.com>
2022-12-22 01:58:19 +0000

    Add skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,61 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import meter1 from '../assets/img/meter1.svg';
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+    return(
+        <section className='skill' id='skills'>
+            <Container>
+                <Row>
+                    <Col>
+                    <div className='skill-bx'>
+                        <h2>
+                            Skills
+                        </h2>
+                        <p>Lorem ipsum</p>
+                        <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+                            <div className='item'>
+                                <img src={meter1} alt={"Image"}></img>
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={meter1} alt={"Image"}></img>
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={meter1} alt={"Image"}></img>
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className='item'>
+                                <img src={meter1} alt={"Image"}></img>
+                                <h5>Web Development</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    )
+}
