8b209351fc2692488e590dbbe934e4ed6980a8e0
Bazil <bazil.khan@urbaneyes.ai>
2023-04-01 16:08:15 -0400

    updates navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,7 +39,8 @@ export const NavBar = () => {
     <Navbar expand="lg">
       <Container>
         <Navbar.Brand href="#home">
-            <img src={logo} alt="logo"/>
+            {/* <img src={logo} alt="Welcome"/> */}
+            <Nav.Link href="#home" className={activeLink == 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Welcome</Nav.Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span class="navbar-toggler-icon"></span>
@@ -49,12 +50,14 @@ export const NavBar = () => {
             <Nav.Link href="#home" className={activeLink == 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
             <Nav.Link href="#skills" className={activeLink == 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
             <Nav.Link href="#projects" className={activeLink == 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
+            <Nav.Link href="#blog" className={activeLink == 'blog' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('blog')}>Blog (coming soon)</Nav.Link>
 
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt=""></img></a>
-                <a href="#"><img src={navIcon2} alt=""></img></a>
+              <a href="https://www.linkedin.com/in/
+bazilkhan" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt=""></img></a>
+              <a href="https://github.com/yobazy" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt=""></img></a>
             </div>
             <button className="vvd" onClick={() => console.log('connect')}><span>Let's connect</span></button>
           </span>
