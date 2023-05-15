f0d19a5e49095c5b0c82944e6b16cbbac2caa063
baz <bazil.khan@urbaneyes.ai>
2023-05-15 06:21:27 -0400

    fixes all item icons and imports

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,59 +1,109 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import meter1 from '../assets/img/meter1.svg';
-import colorSharp from '../assets/img/color-sharp.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPython, faGitAlt, faJsSquare, faHtml5, faCss3Alt, faReact, faNodeJs, faAutoprefixer, faCuttlefish, faConnectdevelop } from '@fortawesome/free-brands-svg-icons';
+import { faServer, faCode, faDatabase, faFlask, faRocket, faBook, faMagnifyingGlassChart } from '@fortawesome/free-solid-svg-icons';
+
 
 export const Skills = () => {
+    const iconSize = "3x"
     const responsive = {
         superLargeDesktop: {
-          // the naming can be any, depends on you.
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
+            breakpoint: { max: 3000, min: 1024 },
+            items: 5
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
+            breakpoint: { max: 1024, min: 464 },
+            items: 3
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
+            breakpoint: { max: 464, min: 0 },
+            items: 1
         }
-      };
+    };
 
-    return(
+    return (
         <section className='skill' id='skills'>
             <Container>
                 <Row>
                     <Col>
-                    <div className='skill-bx'>
-                        <h2>
-                            Skills
-                        </h2>
-                        <p>Lorem ipsum</p>
-                        <Carousel responsive={responsive} infinite={true} className='skill-slider'>
-                            <div className='item'>
-                                <img src={meter1} alt={"Image"}></img>
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className='item'>
-                                <img src={meter1} alt={"Image"}></img>
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className='item'>
-                                <img src={meter1} alt={"Image"}></img>
-                                <h5>Web Development</h5>
-                            </div>
-                            <div className='item'>
-                                <img src={meter1} alt={"Image"}></img>
-                                <h5>Web Development</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+                        <div className='skill-bx'>
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>Lorem ipsum</p>
+                            <Carousel responsive={responsive} infinite={true} className='skill-slider'>
+
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faReact} size={iconSize} />
+                                    <h5>ReactJS</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faJsSquare} size={iconSize} />
+                                    <h5>JavaScript (+Typescript)</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faPython} size={iconSize} />
+                                    <h5>Python</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faDatabase} size={iconSize} />
+                                    <h5>PostgreSQL</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faGitAlt} size={iconSize} />
+                                    <h5>Git/Github</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faConnectdevelop} size={iconSize} />
+                                    <h5>Express</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faHtml5} size={iconSize} />
+                                    <h5>HTML</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faCss3Alt} size={iconSize} />
+                                    <h5>CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faMagnifyingGlassChart} size={iconSize} />
+                                    <h5>TensorFlow</h5>
+                                </div>
+                                <div className="item">
+                                <FontAwesomeIcon icon={faFlask} size={iconSize} />
+                                    <h5>JupyterLab</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faBook} size={iconSize} />
+                                    <h5>Notion</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faServer} size={iconSize} />
+                                    <h5>RubyonRails</h5>
+                                </div>
+
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faCode} size={iconSize} />
+                                    <h5>REST API</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faNodeJs} size={iconSize} />
+                                    <h5>NextJS</h5>
+                                </div>
+                                <div className="item">
+                                    <FontAwesomeIcon icon={faRocket} size={iconSize} />
+                                    <h5>Vercel</h5>
+                                </div>
+
+                            </Carousel>
+                        </div>
                     </Col>
                 </Row>
             </Container>
