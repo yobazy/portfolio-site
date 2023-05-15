ccc5b700cde777552911e0c8306b2f648da65f97
baz <bazil.khan@urbaneyes.ai>
2023-05-15 06:39:56 -0400

    removes lorem ipsum text

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -37,7 +37,7 @@ export const Skills = () => {
                             <h2>
                                 Skills
                             </h2>
-                            <p>Lorem ipsum</p>
+                            {/* <p>Lorem ipsum</p> */}
                             <Carousel responsive={responsive} infinite={true} className='skill-slider'>
 
                                 <div className="item">
