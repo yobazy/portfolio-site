41df2ec262ea225e9dd0bdba39c72c14b3b52578
baz <bazil.khan@urbaneyes.ai>
2023-05-15 05:54:25 -0400

    updates project display cards

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,19 @@
-import { Col } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 
-export const ProjectCard = ({title, description, imgURL}) => {
-    return(
-        <Col sm={6} md={4}>
-            <div className="proj-imgbx">
-                <img src={imgURL}/>
-                <div className='proj-txt'>
-                    <h4>{title}</h4>
-                    <span>{description}</span>
-                </div>
-            </div>
-        </Col>
+export const ProjectCard = ({ title, description, skills, imgURL }) => {
+    return (
+        <Card className="mb-3" style={{ width: '100%' }}>
+            <Card.Img variant="top" src={imgURL} className="card-img"/>
+            <Card.Body style={{ padding: '1.25rem', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    {description}
+                </Card.Text>
+                <Card.Text>
+                    Skills: {skills}
+                </Card.Text>
+                <Button variant="primary" href="#" target="_blank">View Project</Button>
+            </Card.Body>
+        </Card>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,60 +1,62 @@
-import { Row, Col, Container, Tab, Nav } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import colorSharp2 from '../assets/img/color-sharp2.png';
-import projImg from '../assets/img/color-sharp2.png'
+import projImg from '../assets/img/color-sharp2.png';
+import Carousel from "react-multi-carousel";
 
 export const Projects = () => {
-    
+    const responsive = {
+        superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
+        desktop: { breakpoint: { max: 3000, min: 1024 }, items: 3 },
+        tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+        mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+    };
+
     const projects = [
         {
-            title: 'Project1',
-            description: 'Design and Development',
+            title: 'Festify',
+            description: 'A React app which curates personalized Spotify playlists featuring artists from music events.',
+            skills: 'javascript',
             imgURL: projImg,
-        }
+        },
+        {
+            title: 'Festify',
+            description: 'A React app which curates personalized Spotify playlists featuring artists from music events.',
+            skills: 'javascript',
+            imgURL: projImg,
+        },
+        {
+            title: 'Festify',
+            description: 'A React app which curates personalized Spotify playlists featuring artists from music events.',
+            skills: 'javascript',
+            imgURL: projImg,
+        },
+        // add more unique projects
     ]
+
+    const projectCards = projects.length > 0 ? (
+        projects.map((project, index) => (
+            <div className='carousel-item-padding' key={index}>
+                <ProjectCard {...project} />
+            </div>))
+    ) : (
+        <p>No projects to display at this time.</p>
+    );
 
     return(
         <section className='project' id='projects'>
-        <Container>
-            <Row>
-                <Col>
-                    <h2>Projects</h2>
-                    <p>Lorem ipsum</p>
-                    <Tab.Container id="project-tabs" defaultActiveKey={"first"}>
-                    <Nav variant="pills" defaultActiveKey='/home' className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                        <Nav.Item>
-                        <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                        <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                        <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                        </Nav.Item>
-                  </Nav>
-                  </Tab.Container>
-                  <Tab.Content>
-                    <Tab.Pane eventKey="first">
-                        <Row>
-                            {
-                                projects.map((project, index) => {
-                                    return (
-                                        <ProjectCard
-                                            key={index}
-                                            {...project}
-                                            />
-                                    )
-                                })
-                            }
-                        </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">Lorem ipsum</Tab.Pane>
-                    <Tab.Pane eventKey="third">Lorem ipsum</Tab.Pane>
-                  </Tab.Content>
-                </Col>
-            </Row>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="project-bx">
+                            <h2>Projects</h2>
+                            <p>Here are some of the projects I've worked on:</p>
+                            <Carousel responsive={responsive} infinite={true} className='project-slider'>
+                                {projectCards}
+                            </Carousel>
+                        </div>
+                    </Col>
+                </Row>
             </Container>
-            <img className="background-image-right" src={colorSharp2}/>
         </section>
     )
 }
