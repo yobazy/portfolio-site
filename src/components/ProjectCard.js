ce1aa85a017badea88b9183a5c0c41f67d48ff15
baz <bazil.khan@urbaneyes.ai>
2023-05-16 22:17:46 -0400

    change card colors

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,9 +2,9 @@ import { Card, Button } from 'react-bootstrap';
 
 export const ProjectCard = ({ title, description, skills, url, imgURL }) => {
     return (
-        <Card className="mb-3 project-card" style={{ width: '100%' }}>
+        <Card className="mb-3 project-card" style={{ width: '100%', backgroundColor: '#0f171f' }}>
             <Card.Img variant="top" src={imgURL} className="card-img" />
-            <Card.Body>
+            <Card.Body className="card-body">
                 <div className='project-info'>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>
