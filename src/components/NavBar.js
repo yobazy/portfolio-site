42796d2d3c76a572538d2cec10e50eed8dbe5476
Bazil <king.bazil.khan@gmail.com>
2025-03-28 14:01:31 -0400

    navbar updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 
 export const NavBar = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
   const [activeLink, setActiveLink] = useState(location.pathname);
 
   useEffect(() => {
     const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
+      setScrolled(window.scrollY > 50);
     }
 
     window.addEventListener("scroll", onScroll);
@@ -29,78 +22,83 @@ export const NavBar = () => {
     setActiveLink(location.pathname);
   }, [location]);
 
-  const onUpdateActiveLink = (value) => {
-    setActiveLink(value);
-  }
-
   return (
-    <Navbar expand="lg" className="navbar-backdrop">
-      <Container>
-        <Navbar.Brand as={Link} to="/">
-          {/* <img src={logo} alt="Welcome"/> */}
-          <span className={activeLink === '/' ? 'active navbar-link' : 'navbar-link'} 
-                style={{ color: 'white' }}>
+    <nav className={`navbar ${scrolled ? 'scrolled' : ''}`}>
+      <div className="navbar-container">
+        <Link to="/" className="navbar-brand">
+          <span className={activeLink === '/' ? 'active navbar-link' : 'navbar-link'}>
             Welcome
           </span>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link 
-              as={Link} 
+        </Link>
+
+        <button 
+          className={`navbar-toggle ${isMenuOpen ? 'active' : ''}`}
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <div className={`navbar-content ${isMenuOpen ? 'active' : ''}`}>
+          <div className="nav-links">
+            <Link 
               to="/" 
               className={activeLink === '/' ? 'active' : ''}
-              onClick={() => onUpdateActiveLink('/')}
+              onClick={() => setIsMenuOpen(false)}
             >
               Home
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
+            </Link>
+            <Link 
               to="/projects" 
               className={activeLink === '/projects' ? 'active' : ''}
-              onClick={() => onUpdateActiveLink('/projects')}
+              onClick={() => setIsMenuOpen(false)}
             >
               Projects
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
+            </Link>
+            <Link 
               to="/media" 
               className={activeLink === '/media' ? 'active' : ''}
-              onClick={() => onUpdateActiveLink('/media')}
+              onClick={() => setIsMenuOpen(false)}
             >
               Media
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
+            </Link>
+            <Link 
               to="/blog" 
               className={activeLink === '/blog' ? 'active' : ''}
-              onClick={() => onUpdateActiveLink('/blog')}
+              onClick={() => setIsMenuOpen(false)}
             >
               Blog
-            </Nav.Link>
-            <Nav.Link 
-              as={Link} 
+            </Link>
+            <Link 
               to="/about"
               className={activeLink === '/about' ? 'active' : ''}
-              onClick={() => onUpdateActiveLink('/about')}
+              onClick={() => setIsMenuOpen(false)}
             >
               About
-            </Nav.Link>
-          </Nav>
-          <span className="navbar-text">
-            <div className="social-icon">
-              <a href="https://www.linkedin.com/in/bazilkhan" target="_blank" rel="noopener noreferrer">
-                <img src={navIcon1} alt="LinkedIn" />
-              </a>
-              <a href="https://github.com/yobazy" target="_blank" rel="noopener noreferrer">
-                <img src={navIcon2} alt="GitHub" className="nav-icon" />
-              </a>
-            </div>        
-          </span>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            </Link>
+          </div>
+
+          <div className="social-icons-container">
+            <a 
+              href="https://www.linkedin.com/in/bazilkhan" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="social-icon-link"
+            >
+              <img src={navIcon1} alt="LinkedIn" className="social-icon-img" />
+            </a>
+            <a 
+              href="https://github.com/yobazy" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="social-icon-link"
+            >
+              <img src={navIcon2} alt="GitHub" className="social-icon-img" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
